f4056013845cbce8cb8a5501839b1283389f2667
mariamaynard <maria.maynard213@gmail.com>
2020-06-05 15:31:00 +0000

    Adding features to make the comments sections pretty and adding a sort feature

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -23,6 +23,7 @@ import com.google.appengine.api.datastore.Query.SortDirection;
 import java.util.ArrayList;
 import com.google.gson.Gson;
 import java.util.List;
+import com.google.sps.data.Comment;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -36,6 +37,7 @@ public class DataServlet extends HttpServlet {
 
   DatastoreService datastore;
   int max;
+  String sort;
 
   @Override
   public void init() {
@@ -45,36 +47,55 @@ public class DataServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     //get the comments from the data store sorted by their time stamp so they will show up in order
-    Query query = new Query("Task").addSort("timestamp", SortDirection.DESCENDING);
+    sort = request.getParameter("sort");
+    Query query = getSort();
     // check how many comments we want to show
     String num = request.getParameter("commNum");
     max = Integer.parseInt(num);
     PreparedQuery results = datastore.prepare(query);
-    List<String> cmntsList = new ArrayList<>();
+    List<Comment> cmntsList = new ArrayList<>();
     // return the correct number of comments
     for (Entity entity : results.asIterable(FetchOptions.Builder.withLimit(max))) {
-      String comment = (String) entity.getProperty("comment");
+      Comment comment = new Comment((String)entity.getProperty("name"), (String)entity.getProperty("comment"));
       cmntsList.add(comment);
     }
     Gson gson = new Gson();
     response.setContentType("application/json;");
+    response.setCharacterEncoding("UTF-8");
     response.getWriter().println(gson.toJson(cmntsList));
   }
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     String text = request.getParameter("comment");
+    String name = request.getParameter("name");
     long timestamp = System.currentTimeMillis();
     if(text != null){
       //store the comment as an entity with the timestamp and the comment
       Entity taskEntity = new Entity("Task");
       taskEntity.setProperty("timestamp", timestamp);
       taskEntity.setProperty("comment", text);
+      taskEntity.setProperty("name", name);
       //add the comment to the datastore so that we can get it back later
       DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
       datastore.put(taskEntity);
     }
+    // let the next thing know that this method is done
     response.setContentType("text/html");
     response.getWriter().println("done");
+  }
+
+  private Query getSort() {
+    Query query;
+    if(sort.equals("Newest")) {
+      query = new Query("Task").addSort("timestamp", SortDirection.DESCENDING);
+    } else if(sort.equals("Oldest")) {
+      query = new Query("Task").addSort("timestamp", SortDirection.ASCENDING);
+    } else if (sort.equals("A")){
+      query = new Query("Task").addSort("name", SortDirection.ASCENDING);
+    } else {
+      query = new Query("Task").addSort("name", SortDirection.DESCENDING);
+    }
+    return query;
   }
 }
